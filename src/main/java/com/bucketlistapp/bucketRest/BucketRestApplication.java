3b47dfd142ffe88abdb5e3054eeb87aa4d23dcd3
Paul Joseph Devlin <devunderslash@users.noreply.github.com>
2021-12-24 10:32:13 +0000

    initial commit

---
+++ src/main/java/com/bucketlistapp/bucketRest/BucketRestApplication.java
@@ -0,0 +1,13 @@
+package com.bucketlistapp.bucketRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BucketRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bucketlistapp/bucketRest/BucketRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bucketlistapp.bucketRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BucketRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
